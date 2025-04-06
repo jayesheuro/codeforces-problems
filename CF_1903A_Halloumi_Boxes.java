1f108b13f8cad066711011056e322c062288c772
Jayesh <jayehssingh001@gmail.com>
2025-04-06 20:24:33 +0530

    renamed file with a proper name

--- CF_1903A_Halloumi_Boxes.java
+++ CF_1903A_Halloumi_Boxes.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Main {
+public class CF_1903A_Halloumi_Boxes {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
